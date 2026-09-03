a2a0f11cef64b9e4f552088f8a8374ccb25d0e36
Otavio Costa <dev.otaviocostao@gmail.com>
2026-09-02 22:16:17 -0300

    feat: adicionando a nova rota monitoring

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "../components/MainLayout";
 import Home from "../pages/Home";
 import Reminders from "../pages/Reminders";
+import Monitoring from "../pages/Monitoring";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/monitoring" element={<Monitoring />} />
         <Route path="/reminders" element={<Reminders />} />
         <Route path="/settings" element={<Home />} />
         <Route path="*" element={<Home />} />
