f352c974f609c146ae986459a0210672558437ed
Otavio Costa <dev.otaviocostao@gmail.com>
2026-09-16 22:39:32 -0300

    feat: configurando model e lib de reminders

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
-use tauri_plugin_sql::{Migration, MigrationKind};
+pub mod commands;
+pub mod db;
+pub mod models;
+
+use tauri::Manager;
+use tauri_plugin_sql::Migration;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -17,7 +22,20 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let app_state = db::init_database(app.handle())?;
+            app.manage(app_state);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            commands::reminder::get_reminders,
+            commands::reminder::get_reminder_by_id,
+            commands::reminder::create_reminder,
+            commands::reminder::update_reminder,
+            commands::reminder::delete_reminder,
+            commands::reminder::toggle_reminder_status,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
